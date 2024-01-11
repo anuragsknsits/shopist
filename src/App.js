d47b1ee9f8488bb27077ae1a8891fc72852debc0
anuragsknsits <anuragsknsits@gmail.com>
2024-01-11 17:26:52 +0530

    Header and Routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import AboutUs from './pages/About';
+import {Navbar} from './components/Navbar'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={Home} />
-          <Route path="/about" element={About} />
-          <Route path="/signup" element={SignUp} />
-          {/* Add more routes as needed */}
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element = {<Home/>}></Route>
+        <Route path='about' element = {<AboutUs/>}></Route>
+      </Routes>
+    </>
+    
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-const Home = () => {
+export const Home = () => {
   return (
-    <div>
-      <h2>Welcome to Our Website!</h2>
-      <p>
-        My New Website
-      </p>
-    </div>
-  );
-};
-
-export default Home;
+    <div>Home Page</div>
+  )
+}
