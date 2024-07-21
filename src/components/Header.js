3072d1c93c19adc4f7d6783e96b6bbe131bca376
anuragsknsits <anuragsknsits@gmail.com>
2024-07-21 23:51:34 +0530

    Added Logo for Web App

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import NavigationMenu from './NavigationMenu';
 import AuthLinks from './AuthLinks';
 import { useSelector } from 'react-redux';
 import { selectIsAuthenticated, userName } from '../components/selectors/authSelectors';
-
+import logo from '../img/VKADLogo.jpg';
 
 const Header = ({ onLogout }) => {
   // Get the authentication status from the Redux store
@@ -17,11 +17,20 @@ const Header = ({ onLogout }) => {
       <Navbar expand="lg">
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" aria-label="Toogle Me" />
-          <Navbar.Brand as={Link} to="/">VKAD Associates</Navbar.Brand>
+          <Navbar.Brand as={Link}>
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="VKAD Associates logo"
+            />
+            {' VKAD Associates'}
+          </Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
             {/* Pass authentication status and onLogout function to child components */}
             <NavigationMenu isAuthenticated={isAuthenticated} />
-            <AuthLinks username = {username} isAuthenticated={isAuthenticated} onLogout={onLogout} />
+            <AuthLinks username={username} isAuthenticated={isAuthenticated} onLogout={onLogout} />
           </Navbar.Collapse>
         </Container>
       </Navbar>
