602a8b332717912ae56fcbe64ae2804885739134
anuragsknsits <anuragsknsits@gmail.com>
2024-08-01 01:43:28 +0530

    Sidebar responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,62 @@
-// src/components/Header.js
-import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import NavigationMenu from './NavigationMenu';
 import AuthLinks from './AuthLinks';
 import { selectIsAuthenticated, userName } from '../components/selectors/authSelectors';
 import logo from '../img/VKADLogo.jpg';
+import Sidebar from './Sidebar';
 import '../styles/Layout.css';
 
 const Header = ({ onLogout }) => {
-  // Get the authentication status from the Redux store
   const isAuthenticated = useSelector(selectIsAuthenticated);
   const username = useSelector(userName);
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleShowSidebar = () => setShowSidebar(true);
+  const handleCloseSidebar = () => setShowSidebar(false);
+
+  const updateWindowDimensions = () => {
+    setIsMobile(window.innerWidth <= 992);
+  };
+
+  useEffect(() => {
+    updateWindowDimensions();
+    window.addEventListener('resize', updateWindowDimensions);
+    return () => window.removeEventListener('resize', updateWindowDimensions);
+  }, []);
 
   return (
-    <Navbar expand="lg" className="header" variant="light">
-      <Container>
-        <Navbar.Brand as={Link} to='/'>
-          <img
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="VKAD Associates logo"
-          />
-          {' VKAD Associates'}
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <NavigationMenu isAuthenticated={isAuthenticated} />
-          <AuthLinks username={username} isAuthenticated={isAuthenticated} onLogout={onLogout} />
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="header" variant="light">
+        <Container className="d-flex justify-content-between">
+          {isMobile && (
+            <Button variant="primary" onClick={handleShowSidebar} className="navbar-toggler">
+              ☰
+            </Button>
+          )}
+          <Navbar.Brand as={Link} to='/' className="mx-auto">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="VKAD Associates logo"
+            />
+            {' VKAD Associates'}
+          </Navbar.Brand>
+          {!isMobile && (
+            <Navbar.Collapse id="basic-navbar-nav">
+              <NavigationMenu isAuthenticated={isAuthenticated} />
+              <AuthLinks username={username} isAuthenticated={isAuthenticated} onLogout={onLogout} />
+            </Navbar.Collapse>
+          )}
+        </Container>
+      </Navbar>
+      {isMobile && <Sidebar show={showSidebar} handleClose={handleCloseSidebar} />}
+    </>
   );
 };
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,28 @@
 // src/components/Sidebar.js
 import React from 'react';
-import { Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Offcanvas, Nav } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import NavigationMenu from './NavigationMenu';
+import AuthLinks from './AuthLinks';
+import { selectIsAuthenticated, userName } from '../components/selectors/authSelectors';
+import '../styles/Sidebar.css';
 
+const Sidebar = ({ show, handleClose }) => {
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+  const username = useSelector(userName);
 
-const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <Nav className="flex-column">
-        <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-        <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-        <Nav.Link as={Link} to="/settings">Settings</Nav.Link>
-      </Nav>
-    </div>
+    <Offcanvas show={show} onHide={handleClose} className="sidebar" backdrop={false}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Menu</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="flex-column">
+          <NavigationMenu isAuthenticated={isAuthenticated} />
+          <AuthLinks username={username} isAuthenticated={isAuthenticated} onLogout={handleClose} />
+        </Nav>
+      </Offcanvas.Body>
+    </Offcanvas>
   );
 };
 
